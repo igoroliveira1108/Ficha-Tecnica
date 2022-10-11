fc1a8ffdfb90ba432a34844b0099b4e3d3ba44e2
Igor Oliveira <32284673+igoroliveira1108@users.noreply.github.com>
2022-10-11 20:47:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,263 +1,148 @@
-let buttonApagarFrente =  document.querySelector('#apagarImagemFrente');
-// let buttonApagarCostas =  document.querySelector('#apagarImagemCostas');
-let ulMedidas = document.querySelector('#inputMedidas');
-let ulCores = document.querySelector('#inputCores');
-let ulTecidos = document.querySelector('#inputTecido');
-let ulAviamento = document.querySelector('#inputAviamento');
-let btnAddMedidas = document.querySelector('.addMedida');
-let btnAddCores = document.querySelector('.addCores');
-let btnAddTecidos = document.querySelector('.addTecido');
-let btnAddAviamento = document.querySelector('.addAviamento');
-let titleAv = document.querySelector('.titleAv');
-let titleTec = document.querySelector('.titleTec');
-let titleCor = document.querySelector('.titleCor');
-let frente = document.querySelector('#frente');
-let btnAv = document.querySelector('.btnAv');
-let btnCor = document.querySelector('.btnCor');
-let btnTec = document.querySelector('.btnTec');
-let download = document.querySelector('.download');
-// let costas = document.querySelector('#costas');
-
-window.onload = function() {
-  download.addEventListener('click', ()=> {
-      let invoice = document.querySelector('.invoice');
-      var opt = {
-        margin: [1, 0, 1, 0],
-        filename: document.querySelector('#peca').value,
-        image: { type: 'jpeg', quality: 0.95},
-        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
-    };
-      html2pdf().from(invoice).set(opt).save();
-  });
-};
-
-function readFront(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-  
-      reader.onload = function (e) {
-        frente.setAttribute('src', e.target.result);
-        frente.style.width = '500px';
-        frente.style.height = '550px';
-      };
-  
-      reader.readAsDataURL(input.files[0]);
-    }
-
-    document.querySelector('#frenteinput').style.display = 'none';
-    buttonApagarFrente.style.display = 'block';
-  }
-
-  buttonApagarFrente.addEventListener('click', () => {
-    frente.setAttribute('src', '');
-    frente.width = '0px';
-    frente.height = '0px';
-    document.querySelector('#frenteinput').style.display = 'block';
-    document.querySelector('#frenteinput').value = '';
-    buttonApagarFrente.style.display = 'none';
-  });
-
-  /*
-  function readBack(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-  
-      reader.onload = function (e) {
-        costas.setAttribute('src', e.target.result);
-         costas.style.width = '500px';
-         costas.style.height = '550px';
-      };
-  
-      reader.readAsDataURL(input.files[0]);
-    }
-
-    document.querySelector('#costasinput').style.display = 'none';
-    buttonApagarCostas.style.display = 'block';
-  }
-
-  buttonApagarCostas.addEventListener('click', () => {
-    costas.setAttribute('src', '');
-    costas.width = '0px';
-    costas.height = '0px';
-    document.querySelector('#costasinput').style.display = 'block';
-    document.querySelector('#costasinput').value = '';
-    buttonApagarCostas.style.display = 'none';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.js"></script> 
+    <title>Informações Técnicas</title>
+</head>
+<body>
+<div class="invoice">
+    <div class="containerProducao">
+        <div class="inforoupas">
+            <div class="title center mainTitle">
+                <h2>FICHA TÉCNICA PRODUÇÃO</h2>
+                <i class="fa-solid fa-file-export download"></i>
+            </div>
+            <br>
+            <div class="inputroupas">
+                <div class="inputs">
+                    <label for="empresa">Empresa:</label>
+                    <input type="text" class="input" id="empresa" name="empresa">
+                </div>
+                <div class="inputs">
+                    <label for="peca">Peça:</label>
+                    <input type="text" class="input" id="peca" name="peca">
+                </div>
+                <div class="inputs">
+                    <label for="referencia">Referência:</label>
+                    <input type="text" class="input" id="referencia" name="referencia" >
+                </div>
+                <div class="inputs">
+                    <label for="data">Data:</label>
+                    <input type="text" class="input" id="data" name="data" >
+                </div>
+                <div class="inputs">
+                    <label for="modelista">Modelista:</label>
+                    <input type="text" class="input" id="modelista" name="modelista" >
+                </div>
+                <div class="inputs">
+                    <label for="gradacao">Gradação:</label>
+                    <input type="text" class="input" id="gradacao" name="gradacao">
+                </div>
+            </div>
+            <br>
+            <div class="imagens">
+                <div class="frente">
+                    <br>
+                    <h2 class="center">Costa / Frente</h2>
+                    <input type="file" id="frenteinput" onchange="readFront(this);" accept="image/png, image/jpeg">
+                    <br>
+                    <img id="frente" src="#" />
+                    <i id="apagarImagemFrente" style="display: none;" class="fa-solid fa-trash center"></i>
+                </div>  
+                
+                <!-- <div class="costas">
+                    <br>
+                    <h3 class="center">Costas</h3>
+                    <input type="file" id="costasinput" onchange="readBack(this);" accept="image/png, image/jpeg">
+                    <br>
+                    <img id="costas" src="#" />
+                    <i id="apagarImagemCostas" style="display: none;" class="fa-solid fa-trash center"></i> 
+                </div> -->
+            </div>
+            <br>
+            <br>
+            <div class="tables">
+                <div class="divflex">
+                    <div class="title center ">
+                        <h2>FICHA TÉCNICA PRODUÇÃO - ESPECIFICAÇÕES</h2>
+                    </div>
+                        <div class="medidas">
+                            <div class="titleTable">
+                                <i class="fa-solid fa-plus button addMedida"></i>
+                                <h2>Medidas</h2>
+                            </div>
+                            <div id="medidas" >
+                                <ul id="inputMedidas">
+                                    <!-- Adicionar conteúdo aqui dentro-->
+                                </ul>   
+                            </div>
+                        </div>
+                        <div class="aviamentos">
+                            <div class="titleTable">
+                                <i class="fa-solid fa-plus button addAviamento"></i>
+                                <h2>Aviamentos</h2>
+                            </div>
+                            <div class="aviamentos">
+                                <div class="titleAv">
+                                    <h3 class="titleInput">Posição</h3>
+                                    <h3 class="titleInput">Nome</h3>
+                                    <h3 class="titleInput">Tamanho</h3>
+                                    <h3 id="qtd" class="titleInput">Qtd</h3>
+                                    <i class="fa-solid fa-minus btnAv"></i>
+                                </div>
+                                <ul id="inputAviamento">
+                                    <!-- Adicionar conteúdo aqui dentro-->
+                                </ul>   
+                            </div>
+                        </div>
+                        <div class="tecido">
+                            <div class="titleTable">
+                                <i class="fa-solid fa-plus button addTecido"></i>
+                                <h2>Tecido</h2>
+                            </div>
+                            <div class="tecidos">
+                                <div class="titleTec" >
+                                    <h3 class="titleInput">Referência</h3>
+                                    <h3 class="titleInput">Nome</h3>
+                                    <h3 class="titleInput">Composição</h3>
+                                    <h3 id="metragem" class="titleInput">Met</h3>
+                                    <i class="fa-solid fa-minus btnTec"></i>
+                                </div>
+                                <ul id="inputTecido">
+                                    <!-- Adicionar conteúdo aqui dentro-->
+                                </ul>   
+                            </div>
+                        </div>
+                        <div class="medidas">
+                            <div class="titleTable">
+                                <i class="fa-solid fa-plus button addCores"></i>
+                                <h2>Cores</h2>
+                            </div>
+                            <div class="cores">
+                                <div class="titleCor" >
+                                    <h3 class="titleInput">Cor</h3>
+                                    <h3 class="titleInput">Posição</h3>
+                                    <h3 class="titleInput">Qtd Peça</h3>
+                                    <i class="fa-solid fa-minus btnCor"></i>
+                                </div>
+                                <ul id="inputCores">
+                                    <!-- Adicionar conteúdo aqui dentro-->
+                                </ul>   
+                            </div>
+                        </div>
+                        </div>
+            </div>
+        </div>
+        <br>
+    </div>
+</div>
+    <script src="app.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/fontawesome.min.js" integrity="sha512-j3gF1rYV2kvAKJ0Jo5CdgLgSYS7QYmBVVUjduXdoeBkc4NFV4aSRTi+Rodkiy9ht7ZYEwF+s09S43Z1Y+ujUkA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     
-}); */
-
-btnAddMedidas.addEventListener('click', () => {
-
-
-      // Criar li 1
-      let liInput = document.createElement("li");
-      liInput.classList.add("liInput");
-
-      // Criar input
-      let novoInput1 = document.createElement("input");
-      novoInput1.classList.add('inputText');
-      novoInput1.id = 'input1';
-      novoInput1.type="text";
-      liInput.appendChild(novoInput1);
-
-      let novoInput2 = document.createElement("input");
-      novoInput2.classList.add('inputText');
-      novoInput2.id = 'input2';
-      novoInput2.type="text";
-      liInput.appendChild(novoInput2); 
-
-       // Crete button remove
-       let btnRemove = document.createElement('i')
-       btnRemove.classList.add("removeMedida");
-       btnRemove.innerHTML = '<i class="fa-solid fa-minus"></i>';
-       liInput.appendChild(btnRemove)
-
-      ulMedidas.appendChild(liInput)
-
-      btnRemove.addEventListener('click', () => {
-        liInput.remove();
-      })
-});
-
-btnAddCores.addEventListener('click', () => {
-
-  titleCor.style.display = 'flex'
-    
-  // Criar li 1
-  let liInputCores = document.createElement("li");
-  liInputCores.classList.add("liInputCores");
-
-  // Criar input
-  let novoInput1 = document.createElement("input");
-  novoInput1.classList.add('inputTextCores')
-  novoInput1.type="text";
-  liInputCores.appendChild(novoInput1);
-
-  let novoInput2 = document.createElement("input");
-  novoInput2.classList.add('inputTextCores')
-  novoInput2.type="text";
-  liInputCores.appendChild(novoInput2); 
-
-  let novoInput3 = document.createElement("input");
-  novoInput3.classList.add('inputTextCores')
-  novoInput3.type="text";
-  liInputCores.appendChild(novoInput3); 
-
-  // Crete button remove
-  let btnRemove = document.createElement('i')
-  btnRemove.classList.add("removeCores");
-  btnRemove.innerHTML = '<i class="fa-solid fa-minus"></i>';
-  liInputCores.appendChild(btnRemove)
-
-  ulCores.appendChild(liInputCores)
-
-  btnCor.addEventListener('click', () => {
-    titleCor.style.display = 'none';
-  })
-
-  btnRemove.addEventListener('click', () => {
-    liInputCores.remove();
-  })
-   
-});
-
-btnAddTecidos.addEventListener('click', () => {
-
-  titleTec.style.display = 'flex'
-    
-  // Criar li 1
-  let liInputTecidos = document.createElement("li");
-  liInputTecidos.classList.add("liInputTecido");
-
-  // Criar input
-  let novoInput1 = document.createElement("input");
-  novoInput1.classList.add('inputTextTecidos')
-  novoInput1.type="text";
-  liInputTecidos.appendChild(novoInput1);
-
-  let novoInput2 = document.createElement("input");
-  novoInput2.classList.add('inputTextTecidos')
-  novoInput2.type="text";
-  liInputTecidos.appendChild(novoInput2); 
-
-  let novoInput3 = document.createElement("input");
-  novoInput3.classList.add('inputTextTecidos')
-  novoInput3.type="text";
-  liInputTecidos.appendChild(novoInput3); 
-
-  let novoInput4 = document.createElement("input");
-  novoInput4.classList.add('inputTextTecidos')
-  novoInput4.type="text";
-  novoInput4.id = 'qtdTec'
-  liInputTecidos.appendChild(novoInput4);
-
-   // Crete button remove
-   let btnRemove = document.createElement('i')
-   btnRemove.classList.add("removeTecido");
-   btnRemove.innerHTML = '<i class="fa-solid fa-minus"></i>';
-   liInputTecidos.appendChild(btnRemove)
-
-   ulTecidos.appendChild(liInputTecidos)
-
-   btnTec.addEventListener('click', () => {
-    titleTec.style.display = 'none';
-  })
-
-  btnRemove.addEventListener('click', () => {
-    liInputTecidos.remove();
-  })
-   
-});
-
-btnAddAviamento.addEventListener('click', () => {
-
-  titleAv.style.display = 'flex';
-    
-  // Criar li 1
-  let liInputAviamento = document.createElement("li");
-  liInputAviamento.classList.add("liInputAviamento");
-
-  // Criar input
-  let novoInput1 = document.createElement("input");
-  novoInput1.classList.add('inputTextAviamento')
-  novoInput1.type="text";
-  liInputAviamento.appendChild(novoInput1);
-
-  let novoInput2 = document.createElement("input");
-  novoInput2.classList.add('inputTextAviamento')
-  novoInput2.type="text";
-  liInputAviamento.appendChild(novoInput2); 
-
-  let novoInput3 = document.createElement("input");
-  novoInput3.classList.add('inputTextAviamento')
-  novoInput3.type="text";
-  liInputAviamento.appendChild(novoInput3); 
-
-  let novoInput4 = document.createElement("input");
-  novoInput4.classList.add('inputTextAviamento');
-  novoInput4.id = 'qtdAv'
-  novoInput4.type="text";
-  liInputAviamento.appendChild(novoInput4);
-
-   // Crete button remove
-   let btnRemove = document.createElement('i')
-   btnRemove.classList.add("removeAviamento");
-   btnRemove.innerHTML = '<i class="fa-solid fa-minus"></i>';
-   liInputAviamento.appendChild(btnRemove)
-
-   ulAviamento.appendChild(liInputAviamento)
-
-  btnAv.addEventListener('click', () => {
-    titleAv.style.display = 'none';
-  })
-
-  btnRemove.addEventListener('click', () => {
-        liInputAviamento.remove();
-  })
-  
-});
-
-
-
-
-
-
+</body>
+</html>
